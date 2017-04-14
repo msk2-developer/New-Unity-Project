e542a6582b80cf82d840275911bb4957c1fc23f8
MSKMac04 <MSK04@MSK04-no-MacBook-Air.local>
2017-04-14 14:27:34 +0900

    メインに機能追加

---
+++ PedometerApplication/Assets/MainView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainView : MonoBehaviour {
+
+	GameObject petButton;
+
+
+	// Use this for initialization
+	void Start () {
+		// ボタン等の設定
+		petButton = GameObject.Find("PetButton");
+//		petButton.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// ボタン表示、非表示
+//		petButton.SetActive (true);
+	}
+}
